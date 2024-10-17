7ba7999b4af5a099607b80518cf08830f94c5e26
Junio2107 <jtds2107@gmail.com>
2024-10-17 18:53:51 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from datetime import datetime, time
+
+def main():
+    st.title('Sistema de CRM e Vendas da ZapFlow - Frontend Simples')
+    email = st.text_input('Campo de texto para inserção do e-mail do vendedor')
+    data = st.date_input('Data da compra', datetime.now())
+    hora = st.time_input('Hora da compra', value=time(9,0)) # Valor padrão: 09:00
+    valor = st.number_input('Valor da venda', min_value=0.0, format='%.2f')
+    quantidade = st.number_input('Quantidade de produtos', min_value=1,step=1)
+    st.selectbox('Produto',options=['Zapflow com Gemini', 'Zapflow com ChatGPT', 'Zapflow com Llama 3.0'])
+    
+    if st.button('Salvar'):
+        data_hora = datetime.combine(data, hora)
+        st.write('**Dados da Venda:**')
+        st.write(f'Email do vendedor: {email}')
+        st.write(f'Data e hora da compra: {data_hora}')
+        st.write(f'Valor da venda: R${valor:.2f}')
+        st.write(f'Quantidade de produtos: {quantidade}')
+        
+    
+if __name__ == '__main__':
+    main()
